60a9b2e78a8a8448353adbf7b12c103b9c821320
Dimasklog <dimalogachev300907@gmail.com>
2025-11-28 01:05:50 +0300

    frontend

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,11 +1,34 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System.Threading.Tasks;
 
-namespace CrawlBoost;
-
-public partial class MainWindow : Window
+namespace CrawlBoost
 {
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void OpenButton_Click(object? sender, RoutedEventArgs e)
+        {
+            OpenButton.IsEnabled = false;
+            LoadingBar.IsVisible = true;
+
+            await Task.Delay(2000);
+
+            MainScreen.IsVisible = false;
+            SecondScreen.IsVisible = true;
+        }
+
+        private void BackButton_Click(object? sender, RoutedEventArgs e)
+        {
+            SecondScreen.IsVisible = false;
+            MainScreen.IsVisible = true;
+
+            LoadingBar.IsVisible = false;
+            OpenButton.IsEnabled = true;
+        }
     }
 }
