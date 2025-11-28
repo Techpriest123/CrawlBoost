9249a1f627d7a41e94ff8c7aff2126d87e49f976
Dimasklog <dimalogachev300907@gmail.com>
2025-11-28 17:19:44 +0300

    Added some indication

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -64,15 +64,53 @@ namespace CrawlBoost
                     var html = new HtmlDocument();
                     html.LoadHtml(document.DocumentElement.OuterHTML);
                     int metrics = Parser.GetMetrics(html, URL.Text);
-                    SecondWindow.Text = "On Page GEO: " + metrics;
+                    SecondWindow.Text = "Рейтинг GEO: " + metrics;
                     LoadingImage.IsVisible = false;
                 }
             }
-            catch (Exception exception)
+            catch
             {
-                SecondWindow.Text = exception.ToString();
+                SecondWindow.Text = "Invalid URL";
             }
-
+            if (Parser.h1Tags.Count == 1)
+            {
+                ThirdWindow.Text = "Главных заголовков больше чем один, это ухудшает читаемость кода нейросеть";
+            }
+            if (Parser.otherHTags.Count < 1)
+            {
+                FourthWindow.Text = "Других заголовков меньше одного! Использвоание разных заголовков помогает нейросети лучше понять содержание";
+            }
+            if (Parser.metaDescription.Length > 160 || Parser.metaDescription.Length < 120)
+            {
+                FifthWindow.Text = "Тег с описанием должен быть не больше 160 символов, но не меньше 120";
+            }
+            if (Parser.metaTitle.Length > 60 || Parser.metaTitle.Length < 50) 
+            {
+                SixthWindow.Text = "Заголовок не должен быть больше 60 символов, но и не меньше 50";
+            }
+            if (!Parser.containsLang)
+            {
+                SeventhWindow.Text = "Не найдена пометка о языке, без нее нейросети сложнее найти сайт на нужном языке";
+            }
+            if (Parser.imageWithNoAlt)
+            {
+                EighthWindow.Text = "На сайте есть картинка без подписи";
+            }
+        if (Parser.noCanonicalTag)
+            {
+                NinethWindow.Text = "Google рекомендует, чтобы у сайта был у страницы был тег Canonical";
+            }
+        if (!Parser.hasRobots)
+            {
+                TenthWindow.Text = "Боты не могут читать сайт";
+            }
+            if (!Parser.hasJsonLd)
+            {
+                EleventhWindow.Text = "Сайт не предоставляет контекстных тегов для нейросетей";
+            }
+            {
+                
+            }
         }
     }
 }
